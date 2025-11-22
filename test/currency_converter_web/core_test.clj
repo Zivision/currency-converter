3080249f9571003ab845057c1b330e8585f65f41
Zivision <zcastro703@gmail.com>
2025-11-21 19:01:59 -0600

    Added README

---
+++ test/currency_converter_web/core_test.clj
@@ -0,0 +1,7 @@
+(ns currency-converter-web.core-test
+  (:require [clojure.test :refer :all]
+            [currency-converter-web.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
